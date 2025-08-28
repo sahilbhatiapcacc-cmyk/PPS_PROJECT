fa996a6b7e133805780b163f7c1ac1d9f9410aff
sahilbhatiapcacc-cmyk <sahilbhatiapcacc@gmail.com>
2025-08-28 09:52:47 +0530

    Add files via upload

---
+++ Switch.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int day;
+    printf("enter number: ");
+    scanf("%d", &day);
+    switch(day){
+        case 1: printf("monday\n");
+        break;
+        case 2: printf("tuesday\n");
+        break;
+        case 3: printf("wednusday\n");
+        break;
+        case 4: printf("thursday\n");
+        break;
+        case 5: printf("friday\n");
+        break;
+        case 6: printf("saturday\n");
+        break;
+    }
+    return 0;
+    
+}
