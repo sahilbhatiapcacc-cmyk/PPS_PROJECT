4d73d20e414e92a4492cbe3d7881091e7e63c11e
SAHIL BHA7IA <sahilbhatiapcacc@gmail.com>
2025-08-29 11:44:45 +0530

    Create weekdays_name.c

---
+++ weekdays_name.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int x;
+    printf("enter your number for weekdays");
+    scanf("%d",&x);
+    
+    switch(x){
+        case 1: printf("MONDAY");
+        break;
+        case 2: printf("TUESDAY");
+        break;
+        case 3: printf("WEDNUSDAY");
+        break;
+        case 4: printf("THURSDAY");
+        break;
+        case 5: printf("FRIDAY");
+        break;
+        case 6: printf("SATURDAY");
+        break;
+        case 7: printf("SUNDAY");
+        break;
+    }
+    return 0;
+    
+}
